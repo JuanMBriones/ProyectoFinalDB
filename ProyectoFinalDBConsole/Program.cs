4195da79a5a9bd86b1cb1c526fa1cc7090f2c639
JuanBriones <JuanMBriones@outlook.com>
2020-06-09 22:05:12 -0600

    Project

---
+++ ProyectoFinalDBConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ProyectoFinalDBConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection sqlCon = new SqlConnection(@"Data Source=DeathStar\SQLEXPRESS; Initial Catalog=ProyectoFinal;Integrated Security=True;");
+
+            SqlDataAdapter sqlDA = new SqlDataAdapter("SELECT * FROM t_maestros", sqlCon);
+
+            DataTable dTbl = new DataTable();
+            sqlDA.Fill(dTbl);
+            foreach(DataRow row in dTbl.Rows)
+            {
+                Console.WriteLine(row["nomina"]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
